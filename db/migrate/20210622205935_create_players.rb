a9357c253e67e214e081b1d36492ec92d30d799b
Ggs91 <georges_atalla@hotmail.fr>
2021-06-25 21:02:28 +0200

    remove null: false option

--- db/migrate/20210622205935_create_players.rb
+++ db/migrate/20210622205935_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[5.2]
   def change
     create_table :players do |t|
       t.string :name, null: false
-      t.references :game, null: false, foreign_key: true, index: true
+      t.references :game, foreign_key: true, index: true
 
       t.timestamps
     end
