0f244a30e46bb1e70572ed69bc7e4baef6943e1e
Ggs91 <georges_atalla@hotmail.fr>
2021-06-25 20:59:48 +0200

    implemennt GamesController

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,13 +1,36 @@
-module Api
-  module v1
-    class GamesController < ApplicationController
-      def index
-        @games = Game.order(created_at: :desc)
-      end
-
-      def create
-
-      end
+class Api::V1::GamesController < ApplicationController
+  def index
+    @games = Game.order(created_at: :desc)
+  end
+#service Player initializer. on en crée 2 et un par defaut est le bot
+  def create
+    game_summary = PlayRockPaperScissors.new({ #si nom pas bon
+      name: params[:name],
+      move: params[:move]
+    }).perform
+# if round_success?
+      # p 'i'* 60
+      # p game_summary[:winner]
+      # p 'i'* 60
+    game = Game.new
+    game.players << game_summary[:players]
+    game.winner = game_summary[:winner]
+    if game.save
+      render json: {
+        moves: [
+          {
+            name: game.players.first.name,
+            move: game.players.first.hand_move.name
+          },
+          {
+            name: game.players.second.name,
+            move: game.players.second.hand_move.name
+          }
+        ],
+        result: game.result
+      },
+      status: :created
+    else
     end
   end
 end
