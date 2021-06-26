456ff965e5c3106ba8b7a4bf1b239498a1bd7aa9
Ggs91 <georges_atalla@hotmail.fr>
2021-06-26 22:52:27 +0200

    refactor create method using PlayersInitilizer service

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,35 +2,37 @@ class Api::V1::GamesController < ApplicationController
   def index
     @games = Game.order(created_at: :desc)
   end
-#service Player initializer. on en crée 2 et un par defaut est le bot
+
   def create
-    game_summary = PlayRockPaperScissors.new({ #si nom pas bon
-      name: params[:name],
-      move: params[:move]
-    }).perform
-# if round_success?
-      # p 'i'* 60
-      # p game_summary[:winner]
-      # p 'i'* 60
-    game = Game.new
-    game.players << game_summary[:players]
-    game.winner = game_summary[:winner]
-    if game.save
-      render json: {
-        moves: [
-          {
-            name: game.players.first.name,
-            move: game.players.first.hand_move.name
-          },
-          {
-            name: game.players.second.name,
-            move: game.players.second.hand_move.name
-          }
-        ],
-        result: game.result
-      },
-      status: :created
+    initialized_players = PlayersInitializer.new(params).perform
+
+    if initialized_players[:errors].any?
+      render json: result[:errors], status: :unprocessable_entity
     else
+      game = Game.new
+      game.players << [initialized_players[:players][:player], initialized_players[:players][:bot]]
+      game_result = PlayRockPaperScissors.new(initialized_players).perform
+      game.winner = game_result[:winner]
+
+      if game.save
+        render json: {
+          moves: [
+            {
+              name: game.players.first.name,
+              move: game.players.first.hand_move.name
+            },
+            {
+              name: game.players.second.name,
+              move: game.players.second.hand_move.name
+            }
+          ],
+          result: game.result
+        },
+        status: :created
+      else
+        render json: game.errors, status: :unprocessable_entity
+      end
     end
   end
+
 end
