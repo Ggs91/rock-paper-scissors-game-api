fc0cbdd5c634f7956491159bc8456b7950662b9e
Ggs91 <georges_atalla@hotmail.fr>
2021-06-27 01:17:51 +0200

    remove useless seed

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-HandMove.destroy_all
-HandMove.reset_pk_sequence
-HandMove.create(name: 'rock')
-HandMove.create(name: 'paper')
-HandMove.create(name: 'scissors')
-puts "----- #{Handmove.all.size} handmoves seeded -----"
