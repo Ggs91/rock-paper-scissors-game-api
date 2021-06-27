22944623310950c0960c434757e0afbb1ef935e8
Ggs91 <georges_atalla@hotmail.fr>
2021-06-27 14:59:44 +0200

    remove HandMove model

--- app/models/hand_move.rb
+++
@@ -1,5 +0,0 @@
-class HandMove < ApplicationRecord
-  validates :name, inclusion: { in: %w(rock paper scissors),
-    message: "only 'rock', 'paper' and 'scissors' are accepted" }
-  belongs_to :player
-end
