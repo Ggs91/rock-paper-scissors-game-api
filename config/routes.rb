1fd7005e636eb8ef20d544dda9cc046804b29957
Ggs91 <georges_atalla@hotmail.fr>
2021-06-27 16:42:36 +0200

    set defautl format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
       resources :games, only: [:index, :create]
     end
   end
