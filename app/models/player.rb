124cbd10e32ebb4d6bbb1d23e9d60bb1bc4ea324
Ggs91 <georges_atalla@hotmail.fr>
2021-06-27 13:48:06 +0200

    remove requiered option

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
   validates_presence_of :name, presence: true, message: "Please enter a name"
   belongs_to :game, optional: true
-  has_one :hand_move
+  has_one :hand_move, required: true
 end
