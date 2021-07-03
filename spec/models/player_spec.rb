6303d93b2d54e2c8af15def4731a08de028feec5
Ggs91 <georges_atalla@hotmail.fr>
2021-07-03 19:34:14 +0200

    remove useless subject

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Player, type: :model do
   context 'validations' do
-    subject { build(:player) }
-
     it { is_expected.to validate_presence_of(:name)
                     .with_message('Please enter a name') }
     it { is_expected.to validate_inclusion_of(:hand_move)
