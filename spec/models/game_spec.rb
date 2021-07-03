6789e7c189731b61487643f2c3bb5d7f63cc9bdb
Ggs91 <georges_atalla@hotmail.fr>
2021-07-03 20:20:14 +0200

    implement game specs

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to have_many(:players) }
+    it { is_expected.to have_one(:winner).class_name('Player') }
+  end
+
+  describe '#result' do
+    subject { game.result }
+
+    let(:game) { create(:game) }
+
+    context 'when game has no winner' do
+      it { is_expected.to eq('tie') }
+    end
+
+    context 'when game has a winner' do
+      before do
+        game.winner = game.players.first
+      end
+
+      it { is_expected.to eq("#{game.players.first.name} won") }
+    end
+  end
 end
