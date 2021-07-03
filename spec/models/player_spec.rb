48e15b9fccc428cf37d21520fa89e4038d27d651
Ggs91 <georges_atalla@hotmail.fr>
2021-07-03 19:20:22 +0200

    implement validation specs

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    subject { build(:player) }
+
+    it { is_expected.to validate_presence_of(:name)
+                    .with_message('Please enter a name') }
+    it { is_expected.to validate_inclusion_of(:hand_move)
+                    .in_array(%w(rock paper scissors))
+                    .with_message("only 'rock', 'paper' and 'scissors' are accepted") }
+    it { belong_to(:game).optional }
+  end
 end
