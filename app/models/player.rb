3a1ff91d1801ac18a5a22c69a9c7b689eeee2d62
Ggs91 <georges_atalla@hotmail.fr>
2021-07-05 01:21:04 +0200

    capitalize validation message

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   validates_presence_of :name, presence: true, message: "Please enter a name"
   validates :move, inclusion: { in: %w(rock paper scissors),
-    message: "only 'rock', 'paper' and 'scissors' are accepted" }
+    message: "Only 'rock', 'paper' and 'scissors' are accepted" }
 
   belongs_to :game
 
