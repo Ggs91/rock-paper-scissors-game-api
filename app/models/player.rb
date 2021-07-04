317a1cb3465c7a213d84e17b907c74f18d06275d
Ggs91 <georges_atalla@hotmail.fr>
2021-07-04 18:55:59 +0200

    implement #bot

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,19 @@ class Player < ApplicationRecord
   validates_presence_of :name, presence: true, message: "Please enter a name"
   validates :move, inclusion: { in: %w(rock paper scissors),
     message: "only 'rock', 'paper' and 'scissors' are accepted" }
-  belongs_to :game, optional: true
+
+  belongs_to :game
+
+  def bot
+    self.name = 'Bot'
+    self.move = random_move
+
+    self
+  end
+
+  private
+
+  def random_move
+    %w{rock paper scissors}.sample
+  end
 end
