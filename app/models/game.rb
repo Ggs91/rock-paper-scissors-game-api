a857d6de708099e55e61efa02433fc7667d26668
Ggs91 <georges_atalla@hotmail.fr>
2021-07-05 01:20:44 +0200

    add nested attributes for players & validation on player nb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,18 @@
 class Game < ApplicationRecord
   has_many :players
   has_one :winner, class_name: 'Player', foreign_key: 'won_game_id'
+  accepts_nested_attributes_for :players
+  validate :two_players
+
+  NB_PLAYERS = 2
 
   def result
     self.winner.nil? ? 'tie' : "#{self.winner.name} won"
+  end
+
+  private
+
+  def two_players
+    errors.add(:players, 'Two players required') unless self.players.size == NB_PLAYERS
   end
 end
