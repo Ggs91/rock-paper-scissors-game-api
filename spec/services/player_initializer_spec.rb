da83b531646fa6fa70639c4758a184f573ba7e4e
Ggs91 <georges_atalla@hotmail.fr>
2021-07-04 12:14:13 +0200

    use user_params

--- spec/services/player_initializer_spec.rb
+++ spec/services/player_initializer_spec.rb
@@ -1,20 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PlayerInitializer do
+  before(:all) do
+    @initilialized_player
+  end
   subject { PlayerInitializer.new(user_params).perform }
 
-  let(:user_params) { {
-    name: 'Ponky',
-    move: 'rock'
-  } }
+  let(:user_params) do
+    {
+      name: 'Ponky',
+      move: 'rock'
+    }
+  end
   let(:invalid_name) { '' }
   let(:invalid_move) { 'Roq' }
 
 
   describe 'when invalid inputs' do
+
+    it 'returns errors' do
+
+    end
+
     context 'when no name provided' do
       before { user_params[:name] = invalid_name
-               @invalid_player = Player.create(name: user_params[:name], hand_move: user_params[:name])}
+               @invalid_player = Player.create(user_params)
+              }
 
       it { is_expected.to eq(@invalid_player.errors.full_messages) }
     end
