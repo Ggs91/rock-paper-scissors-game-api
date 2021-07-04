3ac2b0d4b0a7cb7ca30d6ba548d909bfb87cfaf2
Ggs91 <georges_atalla@hotmail.fr>
2021-07-05 00:35:57 +0200

    implement normal cases specs

--- spec/services/play_rock_paper_scissors_spec.rb
+++ spec/services/play_rock_paper_scissors_spec.rb
@@ -2,8 +2,39 @@ require 'rails_helper'
 
 RSpec.describe PlayRockPaperScissors do
 
-  subject { PlayRockPaperScissors.new(players).perform }
+  describe 'when game has a winner' do
+    let(:game) { create(:game, :with_players) }
 
-  let(:players) { create(:player) }
+    before do
+      game.players.first.update(move: 'rock')
+      game.players.second.update(move: 'scissors')
+      PlayRockPaperScissors.new(game).perform
+    end
 
+    it "sets game's winner" do
+      expect(game.winner).to eq(game.players.first)
+    end
+
+    it "sets game's played attribute to true" do
+      expect(game.played).to be true
+    end
+  end
+
+  describe 'when tie game' do
+    let(:game) { create(:game, :with_players) }
+
+    before do
+      game.players.first.update(move: 'rock')
+      game.players.second.update(move: 'rock')
+      PlayRockPaperScissors.new(game).perform
+    end
+
+    it "doesn't set game's winner" do
+      expect(game.winner).to be_nil
+    end
+
+    it "sets game's played attribute to true" do
+      expect(game.played).to be true
+    end
+  end
 end
