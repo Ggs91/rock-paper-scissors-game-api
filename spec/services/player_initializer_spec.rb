f25ef807d782cf34aa97feafbd98fe02397508a3
Ggs91 <georges_atalla@hotmail.fr>
2021-07-04 13:15:00 +0200

    implement player_initializer specs

--- spec/services/player_initializer_spec.rb
+++ spec/services/player_initializer_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PlayerInitializer do
-  before(:all) do
-    @initilialized_player
-  end
+
   subject { PlayerInitializer.new(user_params).perform }
 
   let(:user_params) do
@@ -15,23 +13,51 @@ RSpec.describe PlayerInitializer do
   let(:invalid_name) { '' }
   let(:invalid_move) { 'Roq' }
 
+  describe 'when valid input' do
+    it 'persists the player' do
+      expect(subject[:player]).to be_persisted
+    end
+
+    it 'creates player with correct attributes' do
+      expect(subject[:player].name).to eq('Ponky')
+      expect(subject[:player].move).to eq('rock')
+    end
+  end
 
   describe 'when invalid inputs' do
 
-    it 'returns errors' do
+    before { user_params[:name] = invalid_name }
 
+    it "it doesn't persist the player" do
+      expect(subject[:player]).not_to be_persisted
     end
 
-    context 'when no name provided' do
-      before { user_params[:name] = invalid_name
-               @invalid_player = Player.create(user_params)
-              }
-
-      it { is_expected.to eq(@invalid_player.errors.full_messages) }
+    it 'contains an array of errors' do
+      expect(subject[:errors]).not_to be_nil
     end
 
-    context 'when invalid move provided' do
+    describe "array of errors" do
+      context 'when invalid name provided' do
+        let(:player_invalid_name) { Player.create(user_params) }
 
+        it { expect(subject[:errors]).to eq(player_invalid_name.errors.full_messages) }
+      end
+
+      before { user_params[:move] = invalid_move }
+
+      context 'when invalid name & move provided' do
+        let(:player_invalid_name_and_move) { Player.create(user_params) }
+
+        it { expect(subject[:errors]).to eq(player_invalid_name_and_move.errors.full_messages) }
+      end
+
+      before { user_params[:name] = 'Ponky' }
+
+      context 'when invalid move provided' do
+        let(:player_invalid_move) { Player.create(user_params) }
+
+        it { expect(subject[:errors]).to eq(player_invalid_move.errors.full_messages) }
+      end
     end
   end
 end
