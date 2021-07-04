804c35b86eec986b89dbab0d1e5f73a8d1374e8f
Ggs91 <georges_atalla@hotmail.fr>
2021-07-04 16:28:15 +0200

    pluralize players

--- spec/services/play_rock_paper_scissors_spec.rb
+++ spec/services/play_rock_paper_scissors_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PlayRockPaperScissors do
 
-  subject { PlayRockPaperScissors.new(player).perform }
+  subject { PlayRockPaperScissors.new(players).perform }
 
-  let(:player) { create(:player) }
+  let(:players) { create(:player) }
 
 end
