f911ddc4e9ffc6ba018a7ae9415164c826c9d04d
Ggs91 <georges_atalla@hotmail.fr>
2021-07-04 13:49:53 +0200

    rename player_params

--- spec/services/player_initializer_spec.rb
+++ spec/services/player_initializer_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PlayerInitializer do
 
-  subject { PlayerInitializer.new(user_params).perform }
+  subject { PlayerInitializer.new(player_params).perform }
 
-  let(:user_params) do
+  let(:player_params) do
     {
       name: 'Ponky',
       move: 'rock'
@@ -26,7 +26,7 @@ RSpec.describe PlayerInitializer do
 
   describe 'when invalid inputs' do
 
-    before { user_params[:name] = invalid_name }
+    before { player_params[:name] = invalid_name }
 
     it "it doesn't persist the player" do
       expect(subject[:player]).not_to be_persisted
@@ -38,23 +38,23 @@ RSpec.describe PlayerInitializer do
 
     describe 'array of errors' do
       context 'when invalid name provided' do
-        let(:player_invalid_name) { Player.create(user_params) }
+        let(:player_invalid_name) { Player.create(player_params) }
 
         it { expect(subject[:errors]).to eq(player_invalid_name.errors.full_messages) }
       end
 
-      before { user_params[:move] = invalid_move }
+      before { player_params[:move] = invalid_move }
 
       context 'when invalid name & move provided' do
-        let(:player_invalid_name_and_move) { Player.create(user_params) }
+        let(:player_invalid_name_and_move) { Player.create(player_params) }
 
         it { expect(subject[:errors]).to eq(player_invalid_name_and_move.errors.full_messages) }
       end
 
-      before { user_params[:name] = 'Ponky' }
+      before { player_params[:name] = 'Ponky' }
 
       context 'when invalid move provided' do
-        let(:player_invalid_move) { Player.create(user_params) }
+        let(:player_invalid_move) { Player.create(player_params) }
 
         it { expect(subject[:errors]).to eq(player_invalid_move.errors.full_messages) }
       end
